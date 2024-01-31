714c9c14b30342f157b2ba2d280b4a22ee603745
Karl <karl.logon@gmail.com>
2024-01-31 15:33:10 +0800

    play with api

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,23 +1,42 @@
-import { User, columns } from './columns'
-import { DataTable } from '@/components/data-table'
+'use client'
+import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { DataTable } from '@/components/data-table';
+import { columns } from './columns';
 
-async function getUsers(): Promise<User[]> {
-  const res = await fetch(
-    'https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users'
-  )
-  const data = await res.json()
-  return data
-}
+const getReports = async () => {
+  const response = await fetch('http://127.0.0.1:8080/reports');
+  const data = await response.json();
+  return data;
+};
 
-export default async function Page() {
-  const data = await getUsers()
+const ReportTable = () => {
+  const { data, isLoading, isError } = useQuery('reports', getReports);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
 
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='mb-6 text-3xl font-bold'>All Users</h1>
-        <DataTable columns={columns} data={data} />
-      </div>
-    </section>
-  )
-}
+    <div>
+      <h1>Reports</h1>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
+
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReportTable />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
