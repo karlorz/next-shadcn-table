09000c9266f2c4a6549772a0e67c373ba05d05b4
karlorz <karl.logon@gmail.com>
2024-02-02 01:01:31 +0800

    test ReportDetails

--- app/reports/[system]/page.tsx
+++ app/reports/[system]/page.tsx
@@ -6,17 +6,21 @@ import { getReportBySystem } from '@/utils/api-requests';
 import { useQuery } from '@tanstack/react-query';
 
 export default function ReportDetails() {
-
-  const searchParams = useSearchParams();
-  const system = searchParams.get('system'); 
+  const searchParams = useSearchParams()
+  const system = searchParams.get('system')
 
   const { data: report } = useQuery({
     queryKey: ['report', system],
-    queryFn: () => getReportBySystem(system)
-  });
+    queryFn: () => getReportBySystem(system!),
+    enabled: !!system
+  })
+
+  if (!system) {
+    return <div>No system specified</div>
+  }
 
   if (!report) {
-    return <div>Report not found</div>;
+    return <div>Report not found</div>
   }
 
   return (
@@ -26,31 +30,21 @@ export default function ReportDetails() {
       <form>
         <label>
           FDDA1-01
-          <input 
-            type="text"
-            value={report.reports['FDDA1-01']}
-          />
+          <input type='text' value={report.reports['FDDA1-01']} />
         </label>
 
         <label>
-          FDDA1-04  
-          <input
-            type="text"
-            value={report.reports['FDDA1-04']} 
-          />
+          FDDA1-04
+          <input type='text' value={report.reports['FDDA1-04']} />
         </label>
 
         <label>
           FDDA1-05
-          <input 
-            type="text"
-            value={report.reports['FDDA1-05']}
-          />
+          <input type='text' value={report.reports['FDDA1-05']} />
         </label>
 
-        <button type="submit">Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     </div>
-  );
-
+  )
 }
