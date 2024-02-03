c510f903897b48e81b011092afe40538feb60f21
karlorz <karl.logon@gmail.com>
2024-02-04 03:02:07 +0800

    update

--- app/reports/[system]/page.tsx
+++ app/reports/[system]/page.tsx
@@ -1,31 +1,33 @@
 import Link from 'next/link'
 import { getReportBySystem, getSystems, Report } from '@/utils/api-requests'
 
-const systems = getSystems() 
+const systems = getSystems()
 
 export async function generateStaticParams() {
   return systems.map(system => ({
-    system 
+    system
   }))
-} 
+}
 
 interface PageReport extends Omit<Report, 'reports'> {
   reports: {
-    [key: string]: number;
+    [key: string]: number
     // other keys
   }
 }
 
-export default async function Page({ params }: { params: { system: string }}) {
-
-  const report = (await getReportBySystem(params.system)) as PageReport;
-
-  const reports = report.reports as PageReport['reports'];
+export default async function Page({ params }: { params: { system: string } }) {
+  const report = (await getReportBySystem(params.system)) as PageReport
+  const { system, month } = report;
+  const reports = report.reports as PageReport['reports']
 
   return (
     <>
+      <h1>{system} / {month}</h1> 
       {Object.keys(reports).map(code => (
-        <p key={code}>{code}: {reports[code]}</p> 
+        <p key={code}>
+          {code}: {reports[code]}
+        </p>
       ))}
     </>
   )
