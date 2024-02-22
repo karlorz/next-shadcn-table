848afffbc01911a05885d339c3377b66227f2f47
Karl <karl.logon@gmail.com>
2024-02-22 22:24:43 +0800

    night

--- app/(home)/reports/[system]/page.tsx
+++ app/(home)/reports/[system]/page.tsx
@@ -15,47 +15,19 @@ export async function generateStaticParams() {
   }))
 }
 
-// Server-side props
-async function getReports() {
-  const queryClient = new QueryClient()
-
-  // Prefetch systems data
-  // await queryClient.prefetchQuery({
-  //   queryKey: ['systems'],
-  //   queryFn: () => getSystems()
-  // })
-
-  // If system data is available, prefetch Report data
-  const systems = await getSystems()
-  if (systems) {
-    systems.forEach(async system => {
-      await queryClient.prefetchQuery({
-        queryKey: ['getReport'+system, system],
-        queryFn: () => getReportBySystem(system)
-      })
-    })
-  }
-  const dehydratedState = dehydrate(queryClient)
-
-  return dehydratedState
-}
-
 // Page component
 export default async function Page({ params }: { params: { system: string } }) {
   const { system } = params
-  // const dehydratedState = await getReports()
+
   const queryClient = new QueryClient()
-  // if (systems) {
-  //   systems.forEach(async system => {
+
       await queryClient.prefetchQuery({
         queryKey: ['getReport', system],
         queryFn: () => getReportBySystem(system)
       })
-  //   })
-  // }
-  const dehydratedState = dehydrate(queryClient)
+
   return (
-    <HydrationBoundary state={dehydratedState}>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <ListReport system={system} />
     </HydrationBoundary>
   )
